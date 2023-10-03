3f57c91cf6ae0f1c661f12f704b41c2be4407161
Said Kuri <skuri@easyprocess-solutions.com>
2023-10-03 14:12:41 -0600

    [ADD] Conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'OpenSolution Servicios'
+copyright = '2023, OpenSolution'
+author = 'OpenSolution'
 
 release = '0.1'
 version = '0.1.0'
